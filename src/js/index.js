7c3985aaeb6bcaf50a6c004aa7922240be7d8609
gnuoy-oh <oh.my00418@gmail.com>
2022-12-06 18:35:28 +0900

    feat: 컴포넌트, 모듈 컴포넌트 추가 및 스타일 수정

--- src/js/index.js
+++ src/js/index.js
@@ -25,3 +25,74 @@ function requirePngAll(r) {
 
 requirePngAll(require.context("../style-core/images", true, /\.png$/));
 requirePngAll(require.context("../images", true, /\.png$/));
+
+// ****************************
+// UI Event
+// ****************************
+
+const statusElements = document.querySelectorAll(
+  ".pagination__link, .toggle-switch__switch"
+);
+const statusSuffix = "--selected";
+
+const selectElements = document.querySelectorAll(".select");
+const selectSuffix = "--open";
+
+// --selected 추가
+const toggleStatus = () => {
+  statusElements.forEach(i => {
+    i.addEventListener("click", event => {
+      event.preventDefault();
+      let afterSelectButton = i.classList[0].concat(statusSuffix);
+
+      i.classList.toggle(afterSelectButton);
+    });
+  });
+};
+
+//  TODO: check
+const selectOption = optionElement => {
+  const selectBox = optionElement.closest(".select");
+  const selectedElement = selectBox.querySelector(".select-selector__title");
+  selectedElement.textContent = optionElement.textContent;
+  selectedElement.style.color = "#000";
+  selectBox.classList.remove(".select--open");
+};
+
+//  TODO: check
+const detectSelect = () => {
+  selectElements.forEach(element => {
+    element.addEventListener("click", event => {
+      event.preventDefault();
+      console.log("click click");
+      const targetElement = event.target;
+      const isOptionElement = targetElement.classList.contains(
+        "select-container__button"
+      );
+
+      if (isOptionElement) {
+        selectOption(targetElement);
+      }
+    });
+  });
+};
+
+// --open 추가
+const toggleSelect = () => {
+  selectElements.forEach(element => {
+    element.addEventListener("click", event => {
+      event.preventDefault();
+      let afterOpenButton = element.classList[0].concat(selectSuffix);
+
+      element.classList.toggle(afterOpenButton);
+    });
+  });
+};
+
+const init = () => {
+  toggleStatus();
+  toggleSelect();
+  // detectSelect();
+};
+
+init();
